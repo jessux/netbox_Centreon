66a2e8cbac637c1cd3bf2415ee03905ccbeaf2e8
jessux <gabriel.kahlouche@gmail.com>
2020-04-20 11:55:59 +0200

    Correction

--- netbox-Centreon/__init__.py
+++ netbox-Centreon/__init__.py
@@ -1,13 +1,13 @@
 from extras.plugins import PluginConfig
 
 class CentreonConfig(PluginConfig):
-    name = 'netbox_Centreon'
-    verbose_name = 'netbox_Centreon'
+    name = 'netbox-Centreon'
+    verbose_name = 'netbox-Centreon'
     description = 'A plugin for Centreon'
     version = '0.1'
     author = 'Gabriel KAHLOUCHE'
     author_email = 'gabriel.kahlouche@gmail.com'
-    base_url = 'netbox_Centreon'
+    base_url = 'netbox-Centreon'
     required_settings = []
     default_settings = {    }
 
