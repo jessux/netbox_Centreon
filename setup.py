fb8290f950f739b34afd43ef32003a44e141d74b
jessux <gabriel.kahlouche@gmail.com>
2020-04-20 11:33:03 +0200

    Create Setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='netbox-Centreon',
+    version='0.1',
+    description='A NetBox plugin for Centron',
+    url='https://github.com/jessux/netbox-Centreon',
+    author='Gabriel KAHLOUCHE',
+    license='Apache 2.0',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+)
