42662dda9212c853f3ce7ef1a023e3143edfcd1b
jessux <gabriel.kahlouche@gmail.com>
2020-04-20 15:23:08 +0200

    test

--- netbox_Centreon/models.py
+++ netbox_Centreon/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-class CentreonObjectStatus(models.Model):
-    name = models.CharField(max_length=50)
+class CentreonObjectStatus(ipam):
+    ip = models.CharField(max_length=16)
     imported = models.BooleanField(default=False)
     state= models.BooleanField(default=False)
 
     def __str__(self):
-        return self.name
+        return self.ip
 
     def isimported(self):
         return self.imported
