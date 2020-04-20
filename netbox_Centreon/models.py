f86917cd8fc090e6c427667c4ebe9ae7891785c3
jessux <gabriel.kahlouche@gmail.com>
2020-04-20 14:44:56 +0200

    Default

--- netbox_Centreon/models.py
+++ netbox_Centreon/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class CentreonObjectStatus(models.Model):
     name = models.CharField(max_length=50)
-    imported = models.BooleanField
-    state= models.BooleanField
+    imported = models.BooleanField(default=False)
+    state= models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
