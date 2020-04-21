64e622aa8440675f151bbfe1a8eafbb0341520bd
jessux <gabriel.kahlouche@gmail.com>
2020-04-21 13:59:06 +0200

    debug

--- netbox_Centreon/template_content.py
+++ netbox_Centreon/template_content.py
@@ -11,6 +11,7 @@ class SiteCentreonObjectStatus(PluginTemplateExtension):
                 c.setStatus(i)
         return self.render('netbox_Centreon/status.html', extra_context={
             'c': c,
+            'context': self.context
         })
 
 template_extensions = [SiteCentreonObjectStatus]
