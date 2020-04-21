4273f61b18f98b7c3c9fb802e91a81f2da9ffcac
jessux <gabriel.kahlouche@gmail.com>
2020-04-21 14:00:06 +0200

    ad

--- netbox_Centreon/template_content.py
+++ netbox_Centreon/template_content.py
@@ -7,7 +7,7 @@ class SiteCentreonObjectStatus(PluginTemplateExtension):
     def right_page(self):
         c = CentreonObjectStatus()
         for i in CentreonObjectStatus.objects.all():
-            if str(self.context) == i.name:
+            if str(self.context.object) == i.name:
                 c.setStatus(i)
         return self.render('netbox_Centreon/status.html', extra_context={
             'c': c,
