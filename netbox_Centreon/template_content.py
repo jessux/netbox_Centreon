37fa536bd899ff43a02fd6404c8855fc14036bab
jessux <gabriel.kahlouche@gmail.com>
2020-04-23 11:45:38 +0200

    modification template

--- netbox_Centreon/template_content.py
+++ netbox_Centreon/template_content.py
@@ -1,8 +1,57 @@
 from extras.plugins import PluginTemplateExtension
 from .models import CentreonObjectStatus
+import requests
+
 
 class SiteCentreonObjectStatus(PluginTemplateExtension):
     model = 'ipam.ipaddress'
+
+    def getAllHosts(self):
+        payload = {'username': 'gabriel','password': '5UtGqvY5'}
+        response = requests.request("POST", "http://"+config.centreon_url+"/centreon/api/index.php?action=authenticate", data=payload)
+        if response.status_code == 200:
+            url = "http://"+config.centreon_url+"/centreon/api/index.php?action=action&object=centreon_clapi"
+
+            payload = "{\r\n  \"action\": \"show\",\r\n  \"object\": \"host\"\r\n}"
+            headers = {
+                'Content-Type': 'application/json',
+                'centreon-auth-token': response.json()["authToken"],
+                'Content-Type': 'text/plain'
+            }
+
+            response = requests.request("POST", url, headers=headers, data=payload)
+
+            return response.json()
+
+        else:
+            return ""
+
+    def getHostsByName(self,name):
+        payload = {'username': 'gabriel','password': '5UtGqvY5'}
+        response = requests.request("POST", "http://"+config.centreon_url+"/centreon/api/index.php?action=authenticate", data=payload)
+        if response.status_code == 200:
+            url = "http://"+config.centreon_url+"/centreon/api/index.php?action=action&object=centreon_clapi"
+
+            payload = "{\r\n  \"action\": \"show\",\r\n  \"object\": \"host\",\r\n  \"values\": \""+name+"\"\r\n}"
+            headers = {
+                'Content-Type': 'application/json',
+                'centreon-auth-token': response.json()["authToken"],
+                'Content-Type': 'text/plain'
+            }
+
+            response = requests.request("POST", url, headers=headers, data=payload)
+
+            return response.json()
+
+        else:
+            return ""
+
+    def updateStatus(self,object):
+        return
+
+    def buttons(self):
+        print("test")
+        self.getAllHosts()
 
     def right_page(self):
         c = CentreonObjectStatus()
