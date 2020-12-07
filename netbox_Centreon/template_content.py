ef2cc0a3e32492903c6f4d95a60452c9ebf6e9d1
jessux <gabriel.kahlouche@gmail.com>
2020-12-07 16:14:17 +0100

    test template

--- netbox_Centreon/template_content.py
+++ netbox_Centreon/template_content.py
@@ -1,66 +1,66 @@
 from extras import PluginTemplateExtension
 from .models import CentreonObjectStatus
 import requests
-from __init__ import config
+# from __init__ import config
 
 
 class SiteCentreonObjectStatus(PluginTemplateExtension):
     model = 'dcim.devices'
-    centreon_url = getattr(config.required_settings, 'centreon_url', None)
+    
+    # def getAllHosts(self):
+    #     payload = {'username': 'gabriel','password': '5UtGqvY5'}
+    #     response = requests.request("POST", "http://"+self.centreon_url+"/centreon/api/index.php?action=authenticate", data=payload)
+    #     if response.status_code == 200:
+    #         url = "http://"+self.centreon_url+"/centreon/api/index.php?action=action&object=centreon_clapi"
 
-    def getAllHosts(self):
-        payload = {'username': 'gabriel','password': '5UtGqvY5'}
-        response = requests.request("POST", "http://"+self.centreon_url+"/centreon/api/index.php?action=authenticate", data=payload)
-        if response.status_code == 200:
-            url = "http://"+self.centreon_url+"/centreon/api/index.php?action=action&object=centreon_clapi"
+    #         payload = "{\r\n  \"action\": \"show\",\r\n  \"object\": \"host\"\r\n}"
+    #         headers = {
+    #             'Content-Type': 'application/json',
+    #             'centreon-auth-token': response.json()["authToken"],
+    #             'Content-Type': 'text/plain'
+    #         }
 
-            payload = "{\r\n  \"action\": \"show\",\r\n  \"object\": \"host\"\r\n}"
-            headers = {
-                'Content-Type': 'application/json',
-                'centreon-auth-token': response.json()["authToken"],
-                'Content-Type': 'text/plain'
-            }
+    #         response = requests.request("POST", url, headers=headers, data=payload)
 
-            response = requests.request("POST", url, headers=headers, data=payload)
+    #         return response.text.encode("utf-8")
 
-            return response.text.encode("utf-8")
+    #     else:
+    #         return ""
 
-        else:
-            return ""
+    # def getHostsByName(self,name):
+    #     payload = {'username': 'gabriel','password': '5UtGqvY5'}
+    #     response = requests.request("POST", "http://"+self.centreon_url+"/centreon/api/index.php?action=authenticate", data=payload)
+    #     if response.status_code == 200:
+    #         url = "http://"+self.centreon_url+"/centreon/api/index.php?action=action&object=centreon_clapi"
 
-    def getHostsByName(self,name):
-        payload = {'username': 'gabriel','password': '5UtGqvY5'}
-        response = requests.request("POST", "http://"+self.centreon_url+"/centreon/api/index.php?action=authenticate", data=payload)
-        if response.status_code == 200:
-            url = "http://"+self.centreon_url+"/centreon/api/index.php?action=action&object=centreon_clapi"
+    #         payload = "{\r\n  \"action\": \"show\",\r\n  \"object\": \"host\",\r\n  \"values\": \""+name+"\"\r\n}"
+    #         headers = {
+    #             'Content-Type': 'application/json',
+    #             'centreon-auth-token': response.json()["authToken"],
+    #             'Content-Type': 'text/plain'
+    #         }
 
-            payload = "{\r\n  \"action\": \"show\",\r\n  \"object\": \"host\",\r\n  \"values\": \""+name+"\"\r\n}"
-            headers = {
-                'Content-Type': 'application/json',
-                'centreon-auth-token': response.json()["authToken"],
-                'Content-Type': 'text/plain'
-            }
+    #         response = requests.request("POST", url, headers=headers, data=payload)
 
-            response = requests.request("POST", url, headers=headers, data=payload)
+    #         return response.json()
 
-            return response.json()
+    #     else:
+    #         return ""
 
-        else:
-            return ""
+    # def updateStatus(self,object):
+    #     return
 
-    def updateStatus(self,object):
-        return
-
-    def buttons(self):
-        print(self.centreon_url)
-        return "test"
+    # def buttons(self):
+    #     print(self.centreon_url)
+    #     return "test"
 
     def right_page(self):
-        c = CentreonObjectStatus()
-        obj = str(self.context['object']).split("/")[0] if '/' in str(self.context['object']) else str(self.context['object'])
-        for i in CentreonObjectStatus.objects.all():
-            if obj == i.name:
-                c=i
+        # c = CentreonObjectStatus()
+        # obj = str(self.context['object']).split("/")[0] if '/' in str(self.context['object']) else str(self.context['object'])
+        # for i in CentreonObjectStatus.objects.all():
+        #     if obj == i.name:
+        #         c=i
+        c= "a"
         return self.render('netbox_Centreon/status.html', extra_context={
             'c': c,
         })
