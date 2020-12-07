b227ebc22261f87f9475f027fbcf8fe453d2869c
jessux <gabriel.kahlouche@gmail.com>
2020-12-07 16:51:44 +0100

    LOL

--- netbox_Centreon/admin.py
+++ netbox_Centreon/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import CentreonObjectStatus
 
-@admin.register(Centreon)
+@admin.register(CentreonObjectStatus)
 class CentreonAdmin(admin.ModelAdmin):
     list_display = ('name', 'imported','state')
